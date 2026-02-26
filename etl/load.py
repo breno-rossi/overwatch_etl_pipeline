ca05bb330185a0b057931133395b382b4c55a4f0
Breno Rossi <brenorossiduarte@gmail.com>
2026-02-26 20:46:12 -0300

    feat: pipeline etl concluido

--- etl/load.py
+++ etl/load.py
@@ -1,5 +1,5 @@
 import psycopg2
-from transform import transform_dataset
+
 
 
 
@@ -13,10 +13,10 @@ def load_into_db (df):
         password="colorado63")
 
         cursor = conn.cursor()
-        sql_command_insert = "INSERT INTO heroes_stats (hero, pickrate, winrate, platform, gamemode, region, collect_at) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        sql_command_insert = "INSERT INTO heroes_stats (hero, pickrate, winrate, platform, gamemode, region, inserted_at) VALUES (%s, %s, %s, %s, %s, %s, %s)"
 
         for row in df.itertuples():
-            cursor.execute(sql_command_insert, (row.hero, row.pickrate, row.winrate, row.region, row.platform, row.gamemode, row.collect_at))
+            cursor.execute(sql_command_insert, (row.hero, row.pickrate, row.winrate, row.region, row.platform, row.gamemode, row.inserted_at))
         conn.commit()
 
     except Exception as e:
--- etl/transform.py
+++ etl/transform.py
@@ -1,19 +1,13 @@
 import pandas as pd
 from datetime import datetime
-from extract import extract_heroes_data
+
 
 
 def transform_dataset (df, platform, gamemode, region):
     df["platform"] = platform
     df["gamemode"] = gamemode
     df["region"] = region
-    df["collect_at"] = datetime.now()
+    df["inserted_at"] = datetime.now()
     df = df.dropna(subset=["pickrate", "winrate"])
 
     return df
-
-df_raw =extract_heroes_data("pc", "americas","quickplay")
-
-df_transformed = transform_dataset(df_raw,"pc", "americas","quickplay")
-print(df_transformed)
-print(df_transformed.to_string())
